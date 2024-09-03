c3092870e323158b140f5a6c135337ac0b5e2be9
GiulliaZS <giulliatabata@gmail.com>
2024-09-03 09:46:41 -0300

    Arquivo de jogos

---
+++ img/JS Game_files/app.js
@@ -0,0 +1,29 @@
+@@ -0,0 +1,22 @@
+// deixe numeroSecreto = parseInt(Math.random() * 11)
+// deixe tentativas = 1
+// deixe a rampa
+// alert('Boas boas vindas ao jogo do número secreto')
+// enquanto (chute != numeroSecreto) {
+// let chute = prompt('Escolha um número entre 1 e 10')
+// se (chute == numeroSecreto) {
+// quebrar
+// } outro {
+// se (chute > numeroSecreto) {
+// alerta('O número secreto é menor')
+// } outro {
+// alerta('O número secreto é maior')
+// }
+// }
+// tentativas++
+// }
+// deixe palavraTentativa = esforço > 1 ? 'tentativa' : 'tentativa'
+// alert(`O número secreto era ${numeroSecreto} e você acertou com apenas ${tentativas} ${palavraTentativa}`)
+
+* {
+    cor de fundo : preto;
+    imagem de fundo :  url ( './img/bg.png' );
+    background-repeat : sem repetição;
+    tamanho-de-fundo : capa;
+    posição de fundo : centro;
+    opacidade :  0,4 ;
+}
